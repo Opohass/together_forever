90a8e22a445690ba67ee139a02a99241b0fa9128
porat99 <porat216@gmail.com>
2022-08-17 16:45:44 +0300

    main finsui

--- log_in.py
+++ log_in.py
@@ -1,1 +1,28 @@
-print("blah")
+
+
+
+Options= ["1","2"]
+import login_func
+print ("hello")
+print("enter 1. to login and,2. to sign up")
+
+choise = input("Enter your choise: ")
+while choise not in Options:
+    choise = input("Enter your choise: ")
+
+if choise == "1":
+    userName= input("Enter your name: ")
+    userpasswd= input("Enter your password: ")
+    if login_func.signin(userName,userpasswd):
+        print("login successful")
+    else: 
+        print("login failed")
+elif choise == "2":
+    userName= input("Enter your name: ")
+    userpasswd= input("Enter your password: ")
+    if login_func.signup(userName,userpasswd):
+        print("login successful")
+    else: 
+        print("login failed")
+else:
+    print("what???!!!")
