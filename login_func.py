042c2c40d8b4c65e7fcead751bfcee772386a9cc
porat99 <porat216@gmail.com>
2022-08-17 16:17:18 +0300

    hi

--- login_func.py
+++ login_func.py
@@ -1,8 +1,8 @@
 
 import json 
-
+import import_export
 def signin(name, password):
-    data={}#yohav func
+    data=pull_data()
     
     if name in data.keys() :
         if data[name] == password :
@@ -12,4 +12,11 @@ def signin(name, password):
     else :
         return False
         
-    
+def signup(name, password):
+    data=pull_data()
+    if name in data.keys() :
+        print("name already exists")
+        return False
+    else:
+        push_data({name: password})
+        return True
