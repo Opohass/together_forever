6dc4810812c7e1cfc473476e8c255a97d925ebfa
porat99 <porat216@gmail.com>
2022-08-17 16:13:43 +0300

    finsh one

---
+++ login_func.py
@@ -0,0 +1,22 @@
+
+import json 
+import import_export
+def signin(name, password):
+    data=pull_data()
+    
+    if name in data.keys() :
+        if data[name] == password :
+            return True
+        else :
+            return False
+    else :
+        return False
+        
+def signup(name, password):
+    data=pull_data()
+    if name in data.keys() :
+        print("name already exists")
+        return False
+    else:
+        push_data({name: password})
+        return True
